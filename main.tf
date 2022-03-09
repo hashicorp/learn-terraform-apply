029892825b2e8bc0e74771ff15da7bde09b063e6
Robin Norwood <rnorwood@hashicorp.com>
2022-03-09 15:26:40 -0600

    versions.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.16.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1.0"
-    }
-
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.7.2"
-    }
-  }
-}
-
 provider "docker" {}
 
 provider "random" {}
